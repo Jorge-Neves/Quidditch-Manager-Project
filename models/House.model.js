d0647762a94e293050af4bc5ee5622fc874ebebe
Nunofgg <goncalvesnuno00@gmail.com>
2021-09-10 11:11:13 +0100

    patch

--- models/House.model.js
+++ models/House.model.js
@@ -3,9 +3,10 @@ const { Schema, model } = require("mongoose");
 
 const houseSchema = new Schema({
       name: String,
+      element: String,
+      // modifiers:{},
+      values:[],
       description: String,
-
-  
 });
 
 const House = model("House", houseSchema);
