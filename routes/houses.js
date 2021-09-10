e51fb469d0e589ac485f3448664d9ece9f6d31d9
Nunofgg <goncalvesnuno00@gmail.com>
2021-09-10 12:29:15 +0100

    houses

--- routes/houses.js
+++ routes/houses.js
@@ -20,63 +20,10 @@ router.get("/houses", async (req, res) => {
 
 
 
-
-
-
-router.get("/houses/create", async (req, res) => {
-    res.render("houses/houses-create")
-});
-
-router.post("/houses/create", async (req, res) => {
-        try{
-        const { name, description } = req.body;
-        await House.create({ name, description });
-        res.redirect("/houses");
-            } catch(e){
-                console.log("error", e)
-            }
-       
-    });
-
-
-
 router.get("/houses/:houseId", async (req, res) => {
     try{
     const houseId = await House.findById(req.params.houseId)
     res.render("houses/houses-details", houseId)
-} catch(e){
-    console.log("error", e)
-}
-});
-
-
-router.get("/houses/:houseId/edit", async (req, res) => {
-    try{
-    const houses = await House.findById(req.params.houseId)
-    res.render("houses/houses-edit", houses);
-} catch(e){
-    console.log("error", e)
-}
-});
-
-router.post("/houses/:houseId/edit", async (req, res) => {
-    const { name, description} = req.body;
-    try{
-     await House.findByIdAndUpdate(req.params.houseId, {
-       name, description
-    });
-    res.redirect(`/houses/${req.params.houseId}`);
-} catch(e){
-    console.log("error", e)
-}
-});
-
-
-
-router.post("/houses/:houseId/delete", async (req, res) => {
-    try{
-     await House.findByIdAndRemove(req.params.houseId);
-    res.redirect("/houses/");
 } catch(e){
     console.log("error", e)
 }
