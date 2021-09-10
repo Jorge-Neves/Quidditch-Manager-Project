9bce9fc86d8fc572be7ec1d4f69f43488c47d57e
Nunofgg <goncalvesnuno00@gmail.com>
2021-09-10 17:26:35 +0100

    tournament and match weather logic

--- routes/tournament.js
+++ routes/tournament.js
@@ -5,100 +5,48 @@ const Student = require("../models/Student.model");
 const Team = require("../models/Team.model.js");
 const Teacher = require("../models/Teacher.model.js");
 const Tournament = require("../models/tournament.model");
+const weather = require("weather-js");
 
-
-
+function winGuessFormula(temperature) {
+  return "Win";
+}
 
 router.get("/tournaments", async (req, res) => {
-    try{
-    const tournaments = await Tournament.find();
+    const citiesArray = ["Lisbon, Portugal", "Berlin, Germany", "Tokio, Japan", "London, United Kingdom", "Buenos Aires, Argentina"]
+const randomCity = citiesArray[Math.floor(Math.random() * citiesArray.length)];
 
-    res.render("tournaments/tournaments-list", {tournaments});
-    } catch(e){
-        console.log("error", e)
+  weather.find(
+    { search: randomCity, degreeType: "C" },
+    function (err, result) {
+      if (err) console.log(err);
+
+      console.log(JSON.stringify(result, null, 2));
+      const temperature = result[0].current.temperature;
+      const humidity = result[0].current.humidity;
+      const skyState = result[0].current.skycode;
+      console.log(temperature);
+      console.log(humidity);
+      console.log(skyState);
+      const winner = winGuessFormula(temperature)
+      Tournament.find().then((tournaments) => {
+        console.log("got weather");
+        res.render("tournaments/tournaments-list", { tournaments });
+      });
     }
+  );
 });
-
 
 router.get("/tournaments/update", (req, res) => {
-   
-    res.render("tournaments/tournaments-update");
-   
-   
+  res.render("tournaments/tournaments-update");
 });
-
-
-
-router.get("/tournaments/create", async (req, res) => {
-    res.render("tournaments/tournaments-create")
-});
-
-router.post("/tournaments/create", async (req, res) => {
-        try{
-        const { name, description } = req.body;
-        await Tournament.create({ name, description });
-        res.redirect("/tournaments");
-            } catch(e){
-                console.log("error", e)
-            }
-       
-    });
-
-
-router.post("/tournaments/create", async (req, res) => {
-
-    const { name, description } = req.body;
-try{
-    await Tournament.create({ name, description });
-    res.redirect("/tournaments/tournaments-create");
-} catch(e){
-    console.log("error", e)
-}
-  
-});
-
-router.get("/tournaments/:tournamentId", async (req, res) => {
-    try{
-    const torunamentId = await Tournament.findById(req.params.tournamentId)
-    res.render("tournaments/tournaments-details", tournamentId)
-} catch(e){
-    console.log("error", e)
-}
-});
-
-
-router.get("/tournaments/:tournamentId/edit", async (req, res) => {
-    try{
-    const tournamnets = await Tournament.findById(req.params.torunamentId)
-    res.render("tournaments/tournaments-edit", tournaments);
-} catch(e){
-    console.log("error", e)
-}
-});
-
-router.post("/tournaments/:tournamentId/edit", async (req, res) => {
-    const { name, description} = req.body;
-    try{
-     await Tournament.findByIdAndUpdate(req.params.tournamentId, {
-       name, description
-    });
-    res.redirect(`/tournamnets/${req.params.tournamentId}`);
-} catch(e){
-    console.log("error", e)
-}
-});
-
-
 
 router.post("/tournaments/:tournamentId/delete", async (req, res) => {
-    try{
-     await Tournament.findByIdAndRemove(req.params.tournamentId);
+  try {
+    await Tournament.findByIdAndRemove(req.params.tournamentId);
     res.redirect("/tournaments/");
-} catch(e){
-    console.log("error", e)
-}
+  } catch (e) {
+    console.log("error", e);
+  }
 });
 
-
 module.exports = router;
-
