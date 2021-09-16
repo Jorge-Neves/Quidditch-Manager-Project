111ee73c37ab8be88593c1bb5a737b80f3f8ef20
Nunofgg <goncalvesnuno00@gmail.com>
2021-09-16 21:03:10 +0100

    last commit of the day

--- routes/tournament.js
+++ routes/tournament.js
@@ -8,23 +8,12 @@ const Tournament = require("../Deprecated/tournament.model");
 
 
 router.get("/tournaments", async (req, res) => {
-      Tournament.find().then((tournaments) => {
-        console.log("got weather");
-        res.render("tournaments/tournaments-list", { tournaments });
-      });
+  res.render("tournaments/tournaments-list");
 });
 
 router.get("/tournaments/update", (req, res) => {
   res.render("tournaments/tournaments-update");
 });
 
-router.post("/tournaments/:tournamentId/delete", async (req, res) => {
-  try {
-    await Tournament.findByIdAndRemove(req.params.tournamentId);
-    res.redirect("/tournaments/");
-  } catch (e) {
-    console.log("error", e);
-  }
-});
 
 module.exports = router;
